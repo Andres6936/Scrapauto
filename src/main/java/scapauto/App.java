04e9751fef174bc783ec25185e4ba14692d72a44
Joan Andrés <andres6936@live.com>
2020-05-12 21:00:34 -0500

    Added new function for avoid the blocking, thanks to Michael Gantman

--- src/main/java/scapauto/App.java
+++ src/main/java/scapauto/App.java
@@ -6,6 +6,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class App
 {
@@ -36,6 +37,8 @@ public class App
         for (String tradeMark : tradeMarks) {
             // Format of URL for the page: https://www.autoevolution.com/$tradeMark$/
             extractInformationOfAutos(URL_OBJECTIVE + tradeMark.replace(" ", "-") + "/");
+            // Wait a small time for avoid will be blocked for the page or the hosting
+            waitFor(10, TimeUnit.SECONDS);
         }
     }
 
@@ -114,6 +117,29 @@ public class App
     {
         for (var tradeMark : tradeMarks) {
             System.out.println(tradeMark);
+        }
+    }
+
+    /**
+     * This method is licensed under The MIT License (MIT) Copyright (c) 2015 Michael Gantman
+     * <br><br>
+     * <p>
+     * This method is a convenience method for sleep that "swallows" {@link InterruptedException}  and
+     * has {@link TimeUnit} parameter in addition to time period so it makes it very convenient. So with
+     * this method there is no need to convert the time into milliseconds. Just simply write
+     * <br><br>
+     *
+     * <p>{@code waitFor(10, TimeUnit.SECONDS);}</p>
+     * <br> No Exception catching needed
+     *
+     * @param period   time period to sleep
+     * @param timeUnit {@link TimeUnit} that specifies in which units the time period is measured
+     */
+    private void waitFor(final int period, TimeUnit timeUnit)
+    {
+        try {
+            timeUnit.sleep(period);
+        } catch (InterruptedException ignored) {
         }
     }
 
