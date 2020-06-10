bcb5869e018663286df09159ec65fad3105c65d7
Joan Andrés <andres6936@live.com>
2020-06-10 10:10:43 -0500

    New class the Clockmaker

--- src/main/java/scapauto/App.java
+++ src/main/java/scapauto/App.java
@@ -35,7 +35,7 @@ public final class App
             // Format of URL for the page: https://www.autoevolution.com/$trade-mark$/
             extractInformationOfAutos(URL_OBJECTIVE, tradeMark);
             // Wait a small time for avoid will be blocked for the page or the hosting
-            waitFor(10, TimeUnit.SECONDS);
+            Clockmaker.waitFor(10, TimeUnit.SECONDS);
         }
     }
 
@@ -72,29 +72,6 @@ public final class App
             printWriter.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-    }
-
-    /**
-     * This method is licensed under The MIT License (MIT) Copyright (c) 2015 Michael Gantman
-     * <br><br>
-     * <p>
-     * This method is a convenience method for sleep that "swallows" {@link InterruptedException}  and
-     * has {@link TimeUnit} parameter in addition to time period so it makes it very convenient. So with
-     * this method there is no need to convert the time into milliseconds. Just simply write
-     * <br><br>
-     *
-     * <p>{@code waitFor(10, TimeUnit.SECONDS);}</p>
-     * <br> No Exception catching needed
-     *
-     * @param period   time period to sleep
-     * @param timeUnit {@link TimeUnit} that specifies in which units the time period is measured
-     */
-    private void waitFor(final int period, TimeUnit timeUnit)
-    {
-        try {
-            timeUnit.sleep(period);
-        } catch (InterruptedException ignored) {
         }
     }
 
