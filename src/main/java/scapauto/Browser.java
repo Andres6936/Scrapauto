a87f82f2c603f18c85e7ed7f7cf5d5b8af515d5c
Joan Andrés <andres6936@live.com>
2020-06-10 09:57:15 -0500

    Get information of engine

--- src/main/java/scapauto/Browser.java
+++ src/main/java/scapauto/Browser.java
@@ -1,12 +1,12 @@
 package scapauto;
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public final class Browser extends WebClient
@@ -35,21 +35,41 @@ public final class Browser extends WebClient
         final Engine engine = new Engine();
         HtmlElement tag = element.getFirstByXPath("//div[@class='title']");
         engine.setName(tag.asText());
-        tag = tag.getFirstByXPath("//dl//dd");
-        engine.setCylinders(tag.asText());
+        final List<HtmlElement> information = tag.getByXPath("//dl//dd");
+        final Iterator<HtmlElement> it = information.iterator();
 
-        DomNode dd = tag.getNextElementSibling();
-        engine.setDisplacement(dd.asText());
-        dd = dd.getNextElementSibling();
-        engine.setPower(dd.asText());
-        dd = dd.getNextElementSibling();
-        engine.setTorque(dd.asText());
-        dd = dd.getNextSibling().getNextSibling();
-        engine.setFuelSystem(dd.asText());
-        dd = dd.getNextSibling();
-        engine.setFuel(dd.asText());
+        engine.setCylinders(it.next().asText());
+
+        if (verifyIfExistInformationOf("Displacement", tag)) {
+            engine.setDisplacement(it.next().asText());
+        } else {
+            engine.setDisplacement("None");
+        }
+
+        if (verifyIfExistInformationOf("Power", tag)) {
+            engine.setPower(it.next().asText());
+        } else {
+            engine.setPower("None");
+        }
+
+        engine.setTorque(it.next().asText());
+        engine.setFuelSystem(it.next().asText());
+        engine.setFuel(it.next().asText());
 
         return engine;
+    }
+
+    private boolean verifyIfExistInformationOf(final String of, final HtmlElement in)
+    {
+        List<HtmlElement> tags = in.getByXPath("//dl[@title='General Specs']//dt");
+
+        for (final var tag : tags) {
+            if (tag.asText().contains(of)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public final Generation getGenerationInformationFrom(final HtmlElement element)
